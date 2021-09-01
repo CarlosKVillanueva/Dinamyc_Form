df0c88dac99bd2ac067b4ab5353279332437d69d
Carlos K Villanueva <carloskvillanueva@gmail.com>
2021-08-31 23:07:28 -0300

    checkbox update

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,39 @@
 
+$('#fechaGrupa').on('change', function () {
+    // let checked = $('input:checked');
+    let checked = new Array();
 
 
-$("#fechaGrupa").on('change', function () {
-    let name = $(" #nameGrupa").val();
-    let selected = $(" #fechaGrupa").val();
+    $.each($("input[name=grupa]:checked"), function() {
+        checked.push($(this).val()); 
+    }); 
+
+    $('.secondaryForm').html('');
     
-    console.log(name)
-    console.log(selected)
-
-    if(selected == "none") {
-        $('.error').show()
-    } else {
-        $('.error').hide()
-        $('.js-sub-form-template').show();
-        $('.formYear').text(selected)
+    $.each(checked, function(key, grupa_value){
+        cloned_html = $('.js-sub-form-template').clone();
+        $(cloned_html).toggle();
+        $(cloned_html).removeClass('js-sub-form-template');
+        $(cloned_html).addClass('js-' + grupa_value + '-input-group')
         
-        $('.sub_form_input').attr("id", name+selected);
+        
+        $('.secondaryForm').append('<h4>' + grupa_value + ' </h4>')
 
-    }
-    
+        //Busca cada elemento que tenga la clase .sub-form-input y lo asigna a input_array
+        input_array = $(cloned_html).find('.sub_form_input')
+
+        //Asigna el Nombre y el valor de la seleccion a cada campo dinamico
+        $.each( input_array, function( key, value ) {
+            value.name = grupa_value;
+            value.id = grupa_value;
+        })
+        //Inserta en la clase .secondaryForm
+        $('.secondaryForm').append(cloned_html)
+        
+    });
+
+
+    // console.log(grupa_value);
+
+   
 })
